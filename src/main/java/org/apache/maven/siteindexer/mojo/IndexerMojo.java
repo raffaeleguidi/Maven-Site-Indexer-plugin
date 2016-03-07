915441995e933605e1fa5b3044057f04a673ced1
Esche <mesche@users.noreply.github.com>
2016-03-07 08:01:25 +0100

    use the File.separator instead of explicit "\" for cross platform (mac os x)

--- src/main/java/org/apache/maven/siteindexer/mojo/IndexerMojo.java
+++ src/main/java/org/apache/maven/siteindexer/mojo/IndexerMojo.java
@@ -1,5 +1,6 @@
 package org.apache.maven.siteindexer.mojo;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -9,26 +10,27 @@ import org.apache.maven.siteindexer.Indexer;
 
 /**
  * This goal will build the index.
- * 
+ *
  * @goal index
  * @aggregator
- * 
+ *
  */
 public class IndexerMojo extends AbstractMojo {
-	
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		Indexer indexer = new Indexer(getLog());
-		try {
-			getLog().info("Maven Site Index");
-			getLog().info("------------------------------");
-			getLog().info("building index.js...");
-			indexer.buildIndex(
-					"target\\site\\", 
-					"target\\site\\js\\index.js");
-			getLog().info("done.");
-		} catch (IOException e) {
-			getLog().error(e);
-		}
-	}
+
+    private static final String FSEP = File.separator;
+    private static final String SITE_PATH = "target" + FSEP + "site" + FSEP;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Indexer indexer = new Indexer(getLog());
+        try {
+            getLog().info("Maven Site Index");
+            getLog().info("------------------------------");
+            getLog().info("building index.js...");
+            indexer.buildIndex(SITE_PATH, SITE_PATH + "js" + FSEP + "index.js");
+            getLog().info("done.");
+        } catch (IOException e) {
+            getLog().error(e);
+        }
+    }
 }
